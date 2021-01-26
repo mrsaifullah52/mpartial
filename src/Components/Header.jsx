c2c9932b25a5e050f23c0922208e898edbadf9ff
Saif Ullah <mrsaifullah52.1@gmail.com>
2021-01-26 20:48:41 +0500

    Header added

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-
+import Video from "../res/video/homepage.mp4"
 
 const Header=()=>{
-  return(<>
-    header
-  </>);
+  return(<header>
+    <h2>Impartial, Remote, 3rd-Party,<br />Estimating Engine</h2>
+    <video src="https://mpartial-data.s3.us-east-2.amazonaws.com/homepage.mp4" preload="auto" loop playsInline autoPlay={true}/>
+  </header>);
 }
 
 export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Navbar from '../Components/Navbar';
+import Header from '../Components/Header';
 
 const Home=()=>{
   return(<>
     <Navbar/>
+    <Header/>
   </>);
 }
 
