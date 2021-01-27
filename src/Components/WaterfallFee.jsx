19f86d7828b417f62866f2bbeb9e31b5135a051e
Saif Ullah <mrsaifullah52.1@gmail.com>
2021-01-27 20:18:36 +0500

    price bar has been added

--- src/Components/WaterfallFee.jsx
+++ src/Components/WaterfallFee.jsx
@@ -1,24 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 
 const WaterfalFee=()=>{
+
+  const [num,setNum]=useState(0);
+
+
+
+
+  const calcNum=(e)=>{
+
+    const selector=document.querySelector(".selector");
+    const progress=document.querySelector(".progress");
+    const card=document.querySelector(".card");
+
+    setNum(e.target.value);
+
+
+    if(num<3){
+      selector.style.left=-1+"%";
+      progress.style.width=0+"%";
+      card.style.left=-1+"%";
+    }else{
+      selector.style.left=num+"%";
+      progress.style.width=num+"%";
+      card.style.left=num+"%";
+    }
+
+  }
+
   return(
     <section className="waterfallfee">
       <div className="container">
+
         <div className="title">
           <h2>Waterfall Fee Structure</h2>
         </div>
+
         <div className="rangbar">
           <div className="card">
-            <b>${100}</b>
+           <div className="body">
+            <b>${num}K</b>
             <p>Estimated Grand Total</p>
+           </div>
           </div>
-          <input type="range" id="waterfallfee"/>
+
+          <input type="range" id="waterfallfee" value={num} onChange={calcNum} min={0} max={100} />
+
+          <div className="progress"></div>
+
+          <div className="selector">
+            <div className="thumb"></div>
+          </div>
+
           <div className="limit">
             <span>$0</span>
             <span>$250,000</span>
             <span>$500,000</span>
+            <span>$750,000</span>
             <span>$1M+</span>
           </div>
+
+
         </div>
       </div>
     </section>
