1ea239ba9aee293441f189232f0ae4775e37205d
Saif Ullah <mrsaifullah52.1@gmail.com>
2021-01-27 16:25:52 +0500

    waterfall fee title has been added

---
+++ src/Components/WaterfallFee.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const WaterfalFee=()=>{
+  return(
+    <section className="waterfallfee">
+      <div className="title">
+        <h2>Waterfall Fee Structure</h2>
+      </div>
+    </section>
+  );
+}
+
+
+export default WaterfalFee;
