3aff93f9b1c98459d74a437c87d54bf2a164e3f3
Saif Ullah <mrsaifullah52.1@gmail.com>
2021-01-28 23:23:23 +0500

    footer left has been added

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,34 @@
 import React from "react";
 
+// icons
+import InstagramIcon from '@material-ui/icons/Instagram'
+import FacebookIcon from '@material-ui/icons/Facebook';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+
 const Footer=()=>{
+
+  const socialicons=[
+    {icon:"<InstagramIcon/>"},
+    {icon:"<FacebookIcon/>"},
+    {icon:"<LinkedInIcon/>"}
+  ]
+
   return(<footer>
     <div className="container">
-
+      <div className="boxs">
+        <div className="box">
+          <p>mpartial, a 4D Schematics Company. Copyright 2020 all rights reserved.</p>
+        </div>
+        <div className="socialicons">
+          <ul>
+            {
+              socialicons.map((icon)=>{
+            <li><a href="#">{icon}</a></li>
+              })
+            }
+          </ul>
+        </div>
+      </div>
     </div>
   </footer>)
 }
